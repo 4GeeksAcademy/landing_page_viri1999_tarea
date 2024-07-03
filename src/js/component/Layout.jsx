9f2ff16cb695092481609ba4ca56addca8192b87
Viridiana De la cruz <167374369+viridlcrloya141999@users.noreply.github.com>
2024-07-03 06:13:41 +0000

    First commit

---
+++ src/js/component/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "./Navbar.jsx"
+
+function Layout() {
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+            </div>
+        </div>
+    )
+}
+
+export default Layout;
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,24 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client";
+import {createRoot} from "react-dom/client";
+import Layout from "./component/Layout.jsx";
+import "../styles/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap"
+//render your react application
+
+createRoot(document.querySelector("#app")).render(<Layout/>)
+
+
+
+
+
+
+
+
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+//import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+//import Home from "./component/home.jsx";
